c70787a713d16d3528cf6949ad3b9e11e5d0cc18
Blazej Osinski <blazej.osinski@gmail.com>
2016-05-24 15:42:41 +0000

    server_setup.sh installs compose and starts ELK

--- server_setup.sh
+++ server_setup.sh
@@ -2,6 +2,15 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
+
+echo "Installing docker compose"
+curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+echo "Start ELK setup"
+docker-compose up -d
+
+echo "Prepare tunnel configuration"
 echo -e "Match User tunnel\n"\
         "  AllowAgentForwarding no\n"\
         "  ForceCommand echo 'This account can only be used for tunnel creation'"\
