622e3a025ad2e862e9912bcfbd58fd605d756ada
Blazej Osinski <blazej.osinski@gmail.com>
2016-07-08 08:43:14 +0000

    - Receiving filebeat messages. - Syslog parsing.

--- client_setup.sh
+++ client_setup.sh
@@ -12,7 +12,7 @@ echo -e "description \"autossh tunnel\"\n"\
 "stop on runlevel [016]\n"\
 "respawn\n"\
 "respawn limit 5 60\n"\
-"exec autossh -M 0 -N -L 5045:localhost:5044 -o \"ServerAliveInterval 30\""\
+"exec autossh -M 0 -N -L 5045:localhost:5044 -L 15044:localhost:15044 -o \"ServerAliveInterval 30\""\
 " -o \"ServerAliveCountMax 2\" -o \"StrictHostKeyChecking=no\" -o \"BatchMode=yes\""\
 " -i /home/ubuntu/tunnelsshkey tunnel@$2" > /etc/init/loggingtunnel.conf
 
